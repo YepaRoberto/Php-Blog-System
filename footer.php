8979fed49e26f47d54396a0fdf37fdfacca9a526
YepaRoberto <72871848+YepaRoberto@users.noreply.github.com>
2023-06-21 12:18:27 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,43 @@
+﻿</div>
+      </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Footer Start -->
+    <div class="container-fluid bg-dark pt-5 px-sm-3 px-md-5 mt-5">
+        <div class="row py-4">
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h5 class="mb-4 text-white text-uppercase font-weight-bold">Get In Touch</h5>
+                <p class="font-weight-medium"><i class="fa fa-map-marker-alt mr-2"></i>123 Street, New York, USA</p>
+                <p class="font-weight-medium"><i class="fa fa-phone-alt mr-2"></i>+53 56553136</p>
+                <p class="font-weight-medium"><i class="fa fa-envelope mr-2"></i>helloword2007@gmail.com</p>
+            </div>
+      
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h5 class="mb-4 text-white text-uppercase font-weight-bold">Flickr Photos</h5>
+                <div class="row">
+                    <div class="col-4 mb-3">
+                        <a href=""><img class="w-100" src="./Assets/company/news-1.jpg" alt=""></a>
+                    </div>
+                    <div class="col-4 mb-3">
+                        <a href=""><img class="w-100" src="./Assets/company/news-2.jpg" alt=""></a>
+                    </div>
+                    <div class="col-4 mb-3">
+                        <a href=""><img class="w-100" src="./Assets/company/news-3.jpg" alt=""></a>
+                    </div>
+                    <div class="col-4 mb-3">
+                        <a href=""><img class="w-100" src="./Assets/company/news-4.jpg" alt=""></a>
+                    </div>
+                    <div class="col-4 mb-3">
+                        <a href=""><img class="w-100" src="./Assets/company/news-5.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid py-4 px-sm-3 px-md-5" style="background: #111111;">
+        <p class="m-0 text-center">&copy; <a href="#">Gamer News</a>. All Rights Reserved. 
+		
+    </div>
+    <!-- Footer End -->
